10c9eb0dd0f1f48efb804251e9fcd127a382289e
meendragon <kimhy1124@soongsil.ac.kr>
2024-12-29 18:32:47 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<utility>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<limits.h>
+#define tp tuple<int,int,int>
+#define pp pair<int, int>
+using namespace std;
+int n,m;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    for(int i = 1;i<=n-m+1;i++){
+        queue<vector<int>> q;
+        vector<int> v;
+        v.push_back(i);
+        q.push(v);
+        while(!q.empty()){
+            vector<int> current = q.front();
+            q.pop();
+            if(current.size() == m){
+                for(int i = 0;i<m;i++){
+                    cout << current[i];
+                    if(i==m-1){
+                        cout << '\n';
+                    }else{
+                        cout << ' ';
+                    }
+                }
+            }else{
+                int last = current.back();
+                for(int next = last+1;next<=n;next++){
+                    vector<int> nextV = current;
+                    nextV.push_back(next);
+                    q.push(nextV);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+
