c641423ddb0238e6fbfadbbee9b5ae378eb7e0ba
meendragon <kimhy1124@soongsil.ac.kr>
2025-01-04 18:16:08 +0900

    [Silver V] Title: 카드1, Time: 208 ms, Memory: 18136 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
+        Queue<Integer> q =  new ArrayDeque<>();
+        for(int i = 0;i<N;i++){
+            q.add(i+1);
+        }
+        for(int i = 0;i<N;i++){
+            System.out.print(q.poll() + " ");
+            if(i ==N-1){
+                break;
+            }
+            Integer top = q.poll();
+            q.add(top);
+        }
+    }
+}
