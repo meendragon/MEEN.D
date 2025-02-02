3d662f692db74aefa857edbab57d3e2b4673f142
meendragon <kimhy1124@soongsil.ac.kr>
2025-02-03 00:00:07 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 26600 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(br.readLine());
+
+            if (temp == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.add(temp);
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
