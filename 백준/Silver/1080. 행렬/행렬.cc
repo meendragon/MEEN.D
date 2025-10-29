4429439a48611d650ae607b520b96fcbea18dc09
meendragon <kimhy1124@soongsil.ac.kr>
2025-10-29 19:53:13 +0900

    [Silver I] Title: 행렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.cc
+++ 백준/Silver/1080. 행렬/행렬.cc
@@ -1,82 +1,70 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n,m;
+
+    int n, m;
     cin >> n >> m;
 
-    vector<vector<bool>> a(n);
-    vector<vector<bool>> b(n);
-    for (int i = 0;i<n;i++) {
+    vector<vector<bool>> a(n, vector<bool>(m));
+    vector<vector<bool>> b(n, vector<bool>(m));
+
+    for (int i = 0; i < n; i++) {
         string temp;
         cin >> temp;
-        for (int j = 0;j<temp.length();j++) {
-            if (temp[j] == '0') {
-                a[i].push_back(false);
-            }else {
-                a[i].push_back(true);
-            }
-        }
-    }  for (int i = 0;i<n;i++) {
-        string temp;
-        cin >> temp;
-        for (int j = 0;j<temp.length();j++) {
-            if (temp[j] == '0') {
-                b[i].push_back(false);
-            }else {
-                b[i].push_back(true);
-            }
+        for (int j = 0; j < m; j++) {
+            a[i][j] = (temp[j] == '1');
         }
     }
-    if (m<3 || n<3) {
-        bool failToSame = false;
-        for (int i = 0;i<n;i++) {
-            for (int j = 0;j<m;j++) {
+
+    for (int i = 0; i < n; i++) {
+        string temp;
+        cin >> temp;
+        for (int j = 0; j < m; j++) {
+            b[i][j] = (temp[j] == '1');
+        }
+    }
+
+    if (n < 3 || m < 3) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (a[i][j] != b[i][j]) {
-                    failToSame = true;
+                    cout << -1;
+                    return 0;
                 }
             }
         }
-        if (failToSame){cout << -1;}
-        else{cout << 0;}
+        cout << 0;
         return 0;
     }
-    int count = 0;
-    for (int i = 0;i<n-2;i++) {
-        for (int j = 0;j<m-2;j++) {
+
+    int cnt = 0;
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = 0; j < m - 2; j++) {
             if (a[i][j] != b[i][j]) {
-                for (int k = 0;k<3;k++) {
-                    for (int l = 0;l<3;l++) {
-                        if (a[i+k][j+l]) {
-                            a[i+k][j+l] = false;
-                        }else {
-                            a[i+k][j+l] = true;
-                        }
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        a[i + x][j + y].flip();
                     }
                 }
-                count++;
+                cnt++;
             }
-
         }
     }
-    bool failToSame = false;
-    for (int i = 0;i<n;i++) {
-        for (int j = 0;j<m;j++) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             if (a[i][j] != b[i][j]) {
-                failToSame = true;
+                cout << -1;
+                return 0;
             }
         }
     }
-    if (failToSame){cout << -1;}
-    else{cout << count;}
 
-
-
+    cout << cnt;
     return 0;
 }
