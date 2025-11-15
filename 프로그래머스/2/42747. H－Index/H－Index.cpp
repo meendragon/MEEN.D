aad048c63521624ce26f6a3df43daae2c6245d74
meendragon <kimhy1124@soongsil.ac.kr>
2025-11-15 11:42:18 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(),citations.end(),[](auto &a, auto& b){
+        return a > b;
+    });
+    int h = 0;
+    for(int i = 0;i<citations.size();i++){
+        if(citations[i] >= i+1){
+            h = i+1;
+        }else{
+            break;
+        }
+    }
+    return h;
+}
